2e176bdde1cc098b2d2f6b9794c52d44abbb57a1
Oleh Kurachenko <oleh.kurachenko@gmail.com>
2018-05-10 17:53:47 +0300

    class Triangle constructors and destructors written

--- src/Triangle.h
+++ src/Triangle.h
@@ -20,29 +20,23 @@ using namespace std;
 
 class Triangle {
 public:
-    Triangle(const double x1=0, const double y1=0,
+    explicit inline Triangle(const double x1=0, const double y1=0,
              const double x2=1, const double y2=0,
              const double x3=0, const double y3=1);
 
-    Triangle(const Point &a, const Point& b, const Point& c);
+    inline Triangle(const Point &a, const Point& b, const Point& c);
 
-    Triangle(const Point &a, const Segment &_bc);
+    inline Triangle(const Point &a, const Segment &bc);
 
-    Triangle (const Triangle&);
+    inline Triangle (const Triangle&);
 
-    ~Triangle();
+    inline ~Triangle();
 
     Triangle& operator=(const Triangle&);
 
     double perimeter() const;
 
     double area() const;
-
-    Point& apexA();
-
-    Point& apexB();
-
-    Point& apexC();
 
     const Point& apexA() const;
 
@@ -76,5 +70,85 @@ private:
 };
 
 inline ostream& operator<<(ostream &, const Triangle &); // TODO implement
+
+//  class Triangle methods definitions
+
+inline Triangle::Triangle(const double x1, const double y1, const double x2, const double y2, const double x3,
+                   const double y3):
+    _a(x1, y1),
+    _b(x2, y2),
+    _c(x3, y3),
+    _ab(NULL),
+    _bc(NULL),
+    _ac(NULL),
+    _ha(NULL),
+    _hb(NULL),
+    _hc(NULL)
+{
+#ifndef NDEBUG
+    cout << "constructor Triangle(" << x1 << ", " << y1 << ", " << x2 << ", " << y2 << ", " << x3
+         << ", " << y3 << ") call, " << *this << " created" << endl;
+#endif
+}
+
+inline Triangle::Triangle(const Point &a, const Point &b, const Point &c):
+        _a(a),
+        _b(b),
+        _c(c),
+        _ab(NULL),
+        _bc(NULL),
+        _ac(NULL),
+        _ha(NULL),
+        _hb(NULL),
+        _hc(NULL)
+{
+#ifndef NDEBUG
+    cout << "constructor Triangle(" << a << ", " << b << ", " << c << ") call, " << *this << " created" << endl;
+#endif
+}
+
+inline Triangle::Triangle(const Point &a, const Segment &bc):
+        _a(a),
+        _b(bc.start()),
+        _c(bc.end()),
+        _ab(NULL),
+        _bc(NULL),
+        _ac(NULL),
+        _ha(NULL),
+        _hb(NULL),
+        _hc(NULL)
+{
+#ifndef NDEBUG
+    cout << "constructor Triangle(" << a << ", " << bc << ") call, " << *this << " created" << endl;
+#endif
+}
+
+inline Triangle::Triangle(const Triangle &that):
+        _a(that.apexA()),
+        _b(that.apexB()),
+        _c(that.apexC()),
+        _ab(NULL),
+        _bc(NULL),
+        _ac(NULL),
+        _ha(NULL),
+        _hb(NULL),
+        _hc(NULL)
+{
+#ifndef NDEBUG
+    cout << "constructor Triangle(" << that << ") call, " << *this << " created" << endl;
+#endif
+}
+
+inline Triangle::~Triangle() {
+#ifndef NDEBUG
+    cout << "constructor ~Triangle(" << *this  << ") call" << endl;
+#endif
+    delete _ab;
+    delete _bc;
+    delete _ac;
+    delete _ha;
+    delete _hb;
+    delete _hc;
+}
 
 #endif //KMA_OOP_04LAB_TRIANGLE_H
